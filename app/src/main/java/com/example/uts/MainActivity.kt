52dc4042ccdd00f2a64dae52b2a65404ec86994a
Rio <rioloka86@gmail.com>
2024-10-25 20:40:27 +0700

    feat: add all buttons logic in main activity

--- app/src/main/java/com/example/uts/MainActivity.kt
+++ app/src/main/java/com/example/uts/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.uts
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener(){
+            if(binding.nameEditText.text.isNotEmpty()){
+                val name =binding.nameEditText.text.toString()
+                val intent = Intent(this, QuizActivity::class.java).apply {
+                    putExtra("name", name)
+                }
+                startActivity(intent)
+            } else{
+                binding.nameEditText.error = "Field name harus diisi!"
+            }
+        }
+
+        binding.buttonStudyMaterials.setOnClickListener(){
+            var uri = "https://developer.android.com/guide/components/intents-filters"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(uri)
+            startActivity(intent)
         }
     }
 }
