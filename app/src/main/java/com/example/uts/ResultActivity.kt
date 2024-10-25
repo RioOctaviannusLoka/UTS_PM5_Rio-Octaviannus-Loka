9aa75bfebe0d893ea028541813dad6db03f57437
Rio <rioloka86@gmail.com>
2024-10-25 23:36:38 +0700

    feat: add result activity logic

--- app/src/main/java/com/example/uts/ResultActivity.kt
+++ app/src/main/java/com/example/uts/ResultActivity.kt
@@ -1,20 +1,45 @@
 package com.example.uts
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.uts.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var name = intent.getStringExtra("name").toString() ?: "User"
+        val Message = getString(R.string.result_message)
+        binding.question.text = "$name $Message"
+
+        binding.buttonReattempt.setOnClickListener{
+            val intent = Intent(this, QuizActivity::class.java).apply {
+                putExtra("name", name)
+            }
+            startActivity(intent)
+        }
+
+        binding.buttonViewAnswers.setOnClickListener{
+            val intent = Intent(this, ViewAnswerActivity::class.java).apply {
+                putExtra("name", name)
+            }
+            startActivity(intent)
+        }
+
+        binding.buttonStudyMaterials.setOnClickListener{
+            val uri = "https://developer.android.com/guide/components/intents-filters"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(uri)
+            startActivity(intent)
         }
     }
 }
